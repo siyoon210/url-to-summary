22e26e22153434bbcf7b76901bd059a5e49c202e
siyoon <siyoon210@gmail.com>
2020-05-12 07:03:51 +0900

    프로젝트 생성

---
+++ src/main/java/me/siyoon/urltosummary/UrlToSummaryApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.urltosummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlToSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlToSummaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/siyoon/urltosummary/UrlToSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.urltosummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlToSummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
