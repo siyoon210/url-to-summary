65294d34291038bf79ffd6ae35247abc328485ef
siyoon <siyoon210@gmail.com>
2020-05-15 09:57:22 +0900

    OpenGraph jar 파일변경, OpenGraph 사용하기

--- src/test/java/me/siyoon/urltosummary/ParserTest.java
+++ src/test/java/me/siyoon/urltosummary/ParserTest.java
@@ -6,18 +6,30 @@ import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.opengraph.OpenGraph;
 
 import java.io.IOException;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
-public class JsoupTest {
+public class ParserTest {
     private final static String URL = "https://sports.news.naver.com/news.nhn?oid=001";
 
     @Test
-    public void test() throws IOException {
+    public void jsoupTest() throws IOException {
         final Document document = Jsoup.connect(URL).get();
         final Elements metaElements = document.head().select("meta");
         final List<String> property = metaElements.eachAttr("property");
+        System.out.println("property = " + property.toString());
+    }
+
+    @Test
+    public void ogTest() throws Exception {
+        OpenGraph testPage = new OpenGraph(URL, true);
+
+        String title = testPage.getContent("title");
+        System.out.println("title = " + title);
+        String type = testPage.getContent("type");
+        System.out.println("type = " + type);
     }
 }
